2b1c374a9057a33bb92ef293e16eb68fbee42f43
Macro303 <Macro303@users.noreply.github.com>
2020-01-10 00:15:52 +1300

    Added more Red Slotted Gems

--- src/main/java/macro/buddy/gems/Acquisition.java
+++ src/main/java/macro/buddy/gems/Acquisition.java
@@ -12,41 +12,41 @@ import java.util.stream.Collectors;
  */
 public class Acquisition {
 	private final List<Recipe> recipes;
-	private final List<Vendor> vendors;
 	private final List<Quest> quests;
+	private final List<Vendor> vendors;
 
 	@JsonCreator
-	public Acquisition(@JsonProperty("recipes") List<Recipe> recipes, @JsonProperty("vendors") List<Vendor> vendors, @JsonProperty("quests") List<Quest> quests) {
+	public Acquisition(@JsonProperty("recipes") List<Recipe> recipes, @JsonProperty("quests") List<Quest> quests, @JsonProperty("vendors") List<Vendor> vendors) {
 		this.recipes = recipes;
-		this.vendors = vendors;
 		this.quests = quests;
+		this.vendors = vendors;
 	}
 
 	public List<Recipe> getRecipes() {
 		return recipes;
 	}
 
-	public List<Vendor> getVendors() {
-		return vendors;
-	}
-
 	public List<Quest> getQuests() {
 		return quests;
+	}
+
+	public List<Vendor> getVendors() {
+		return vendors;
 	}
 
 	public String getDisplay(ClassTag buildClass) {
 		var recipeDisplay = recipes.stream().map(Recipe::getDisplay).collect(Collectors.joining("\n  ", "\n  ", "")).stripTrailing();
 		var vendorDisplay = vendors.stream().filter(it -> it.getClasses().contains(buildClass)).map(Vendor::getDisplay).collect(Collectors.joining("\n  ", "\n  ", "")).stripTrailing();
 		var questDisplay = quests.stream().filter(it -> it.getClasses().contains(buildClass)).map(Quest::getDisplay).collect(Collectors.joining("\n  ", "\n  ", "")).stripTrailing();
-		return String.format("Recipes:%s\nVendors:%s\nQuests:%s", recipeDisplay, vendorDisplay, questDisplay);
+		return String.format("Recipes:%s\nQuests:%s\nVendors:%s", recipeDisplay, questDisplay, vendorDisplay);
 	}
 
 	@Override
 	public String toString() {
 		return "Acquisition{" +
 				"recipes=" + recipes +
-				", vendors=" + vendors +
 				", quests=" + quests +
+				", vendors=" + vendors +
 				'}';
 	}
 }
