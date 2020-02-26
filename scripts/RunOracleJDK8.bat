a6c1067fe5afe8fb21d353fb47ef616639bd4140
Macro303 <Macro303@users.noreply.github.com>
2020-02-26 14:14:29 +1300

    Fix run scripts

--- scripts/RunOracleJDK8.bat
+++ scripts/RunOracleJDK8.bat
@@ -1,5 +1,5 @@
 @echo off
 "C:\Program Files\Java\jre1.8.0_231\bin\java.exe" -version
-"C:\Program Files\Java\jre1.8.0_231\bin\java.exe" -jar Exile-Buddy-0.0.0.jar
+"C:\Program Files\Java\jre1.8.0_231\bin\java.exe" -jar Exile-Buddy.jar
 pause
 exit
--- scripts/RunOracleJDK8.sh
+++ scripts/RunOracleJDK8.sh
@@ -5,6 +5,6 @@ function pause(){
 }
 
 "C:\Program Files\Java\jre1.8.0_231\bin\java.exe" -version
-"C:\Program Files\Java\jre1.8.0_231\bin\java.exe" -jar Exile-Buddy-0.0.0.jar
+"C:\Program Files\Java\jre1.8.0_231\bin\java.exe" -jar Exile-Buddy.jar
 
 pause 'Press <Enter> key to continue...'
