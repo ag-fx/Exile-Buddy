6d32512f58812cef18308822f289f34ee2126154
Macro303 <Macro303@users.noreply.github.com>
2020-07-17 09:56:56 +1200

    Added Config file
    Added Dark mode option

--- src/main/kotlin/github/macro/Launcher.kt
+++ src/main/kotlin/github/macro/Launcher.kt
@@ -1,10 +1,12 @@
 package github.macro
 
+import github.macro.config.Config
 import github.macro.ui.Selector
 import javafx.scene.text.FontWeight
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
 import tornadofx.*
+import java.util.*
 
 /**
  * Created by Macro303 on 2020-Jan-13.
@@ -13,8 +15,12 @@ class Launcher : App(Selector::class, Styles::class) {
 	init {
 		checkLogLevels()
 		LOGGER.info("Welcome to Exile Buddy")
-//		importStylesheet(Launcher::class.java.getResource("Custom-Dark.css").toExternalForm())
-		importStylesheet(Launcher::class.java.getResource("Modena-Dark.css").toExternalForm())
+		if (Config.INSTANCE.useDarkMode) {
+			importStylesheet(Launcher::class.java.getResource("Modena-Dark.css").toExternalForm())
+//		    importStylesheet(Launcher::class.java.getResource("Custom-Dark.css").toExternalForm())
+		}
+		FX.locale = Locale.ENGLISH
+		reloadStylesheetsOnFocus()
 	}
 
 	private fun checkLogLevels() {
---
+++ src/main/kotlin/github/macro/config/Config.kt
@@ -0,0 +1,46 @@
+package github.macro.config
+
+import org.apache.logging.log4j.LogManager
+import org.yaml.snakeyaml.DumperOptions
+import org.yaml.snakeyaml.Yaml
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+
+/**
+ * Created by Macro303 on 2020-Jul-17
+ */
+class Config {
+	var useDarkMode: Boolean = false
+
+	fun save(): Config {
+		Files.newBufferedWriter(Paths.get(filename)).use {
+			it.write(YAML.dumpAsMap(this))
+		}
+		return this
+	}
+
+	companion object {
+		private const val filename = "config.yaml"
+		private val LOGGER = LogManager.getLogger(Config::class.java)
+		private val YAML: Yaml by lazy {
+			val options = DumperOptions()
+			options.defaultFlowStyle = DumperOptions.FlowStyle.FLOW
+			options.isPrettyFlow = true
+			Yaml(options)
+		}
+
+		val INSTANCE: Config by lazy {
+			load()
+		}
+
+		fun load(): Config {
+			val temp = File(filename)
+			if (!temp.exists())
+				Config().save()
+			return Files.newBufferedReader(Paths.get(filename)).use {
+				YAML.loadAs(it, Config::class.java)
+			}.save()
+		}
+	}
+}
